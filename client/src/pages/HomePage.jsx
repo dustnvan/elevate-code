f111b3731e8142fa17867f1539afb23a6920ab9d
dustoonv <dustinvans7@gmail.com>
2025-06-09 20:56:18 -0500

    fixed import casing

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Hero from '../components/Hero';
-import Navbar from '../components/navbar';
+import Navbar from '../components/Navbar';
 import Services from '../components/Services';
 import Footer from '../components/Footer';
 
